2dbe8c5ee00f366ec806954f6c85c1ffaba0ef3f
Megha Sethi <md16289390@gmail.com>
2020-07-17 14:49:36 +0530

    Day 3 Assignmeent

---
+++ Day 3 Assignment/scripts/one.js
@@ -0,0 +1,4 @@
+console.log('Answer 1');
+let num = +prompt('Enter the number to check odd or even');
+let ans = num%2 === 0 ? 'Even' : 'Odd';
+console.log(`The number enetered is ${num} and Number is ${ans}` );
---
+++ Day 3 Assignment/scripts/two.js
@@ -0,0 +1,4 @@
+console.log("Answer 2");
+let user_input = String(prompt(`Enter OS name and version separated by a space.`));
+let user_input_array = user_input.split(" ");
+console.log(`The OS name is '${user_input_array[0]}' and version is '${user_input_array[1]}'.`);
